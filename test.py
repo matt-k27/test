308db5043eb0a6f8776ecc5eed91d7541af3f4f1
Matt <matt@MacBook-Pro-Matt.local>
2024-02-23 12:53:50 +0100

    fffff

---
+++ test.py
@@ -0,0 +1,16 @@
+print(True + 3);
+
+# if(True):
+#     print(True);
+#     print('fff');
+
+
+# if 5 > 10 and 5 < 3:
+#     print('fff')
+
+empty_list = []
+empty_list.insert(10, 5)
+
+print(empty_list)
+
+
